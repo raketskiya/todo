012c82bf2a65ba3aa0b0a5c1bec2f86a005914f9
Anton <antonraketskiy46@gmail.com>
2022-06-06 11:20:29 +0300

    created auth.service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AuthComponent} from './modules/auth/components/auth/auth.component';
+import {TasksComponent} from './modules/tasks/components/tasks/tasks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: AuthComponent},
+  {path:'tasks', component: TasksComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

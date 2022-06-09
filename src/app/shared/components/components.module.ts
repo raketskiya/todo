579810f99029489bb11e80287d4d17f2b6b7dc62
Anton <antonraketskiy46@gmail.com>
2022-06-09 11:09:24 +0300

    deleted auth-dialog

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {HeaderComponent} from './header/header.component';
 import {CommonModule} from '@angular/common';
-import { AuthErrorDialogComponent } from './auth-error-dialog/auth-error-dialog.component';
+
 
 
 @NgModule({
@@ -9,7 +9,6 @@ import { AuthErrorDialogComponent } from './auth-error-dialog/auth-error-dialog.
   exports: [HeaderComponent],
   declarations: [
     HeaderComponent,
-    AuthErrorDialogComponent
   ],
 })
 export class ComponentsModule{
